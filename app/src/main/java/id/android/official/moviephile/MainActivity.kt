7150f5b74c603057ec303de81bf6edca976b97ba
Nicky <71775776+nickylivardo@users.noreply.github.com>
2022-11-01 00:36:24 +0700

    New Project!

---
+++ app/src/main/java/id/android/official/moviephile/MainActivity.kt
@@ -0,0 +1,43 @@
+package id.android.official.moviephile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import id.android.official.moviephile.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Moviephile)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.homeFragment, R.id.listFragment, R.id.profileFragment))
+
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+
+}
